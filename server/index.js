3dbce9d5c47cafe4956f4c75f4cdaeb3d84e835d
Harman <singh9765h@gmail.com>
2023-04-28 12:21:54 +0000

    Docker compose file added

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb://mongo:27017/myFirstDatabase"
+const CONNECTION_URL = "mongodb+srv://socialapp.zkbyytc.mongodb.net/myFirstDatabase"
 const PORT = process.env.PORT || 8000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
