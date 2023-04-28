e6c2201619d7d2f1c6b124d6b6bc156384d44fcc
Harman singh <singh9765h@gmail.com>
2023-04-29 00:41:33 +0530

    Updated ports and edited database connection

--- server/index.js
+++ server/index.js
@@ -1,24 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://harman:Harman@socialapp.zkbyytc.mongodb.net/?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 8000;
+const CONNECTION_URL =
+  "mongodb://harman:Harman@ac-j7m0gk2-shard-00-00.zkbyytc.mongodb.net:27017,ac-j7m0gk2-shard-00-01.zkbyytc.mongodb.net:27017,ac-j7m0gk2-shard-00-02.zkbyytc.mongodb.net:27017/?ssl=true&replicaSet=atlas-hnkae7-shard-0&authSource=admin&retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
